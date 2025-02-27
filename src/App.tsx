43994be6fee703aef8e48730fdbc2c983b54de03
Vinicius Magalhaes <viniciusmagalhaes.vsm@gmail.com>
2025-02-27 19:13:37 -0300

    vai quebra ctz

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import EventView from './pages/public/EventView';
 import ProductCheckout from './pages/public/ProductCheckout';
 import PublicEventView from './pages/public/EventView';
 import { ThemeProvider } from './contexts/ThemeContext';
+import Events from './pages/Events';
 
 // Removidas as importações não utilizadas:
 // - ThemeProvider
@@ -33,7 +34,7 @@ function App() {
           <Route path="/admin" element={<Layout><Dashboard /></Layout>} />
           <Route path="/admin/dashboard" element={<Layout><Dashboard /></Layout>} />
           <Route path="/admin/events-list" element={<Layout><EventsList /></Layout>} />
-          <Route path="/admin/create-event" element={<Layout><CreateEvent /></Layout>} />
+          <Route path="/admin/create-event" element={<Layout><Events /></Layout>} />
           <Route path="/admin/events/:id" element={<Layout><EventDetail /></Layout>} />
           <Route path="/admin/raffles" element={<Layout><Raffles /></Layout>} />
           <Route path="/admin/create-raffle" element={<Layout><CreateRaffle /></Layout>} />
@@ -44,6 +45,21 @@ function App() {
           <Route path="/events/:id" element={<EventView />} />
           <Route path="/events/:id/checkout" element={<ProductCheckout />} />
           <Route path="/events/:eventId" element={<PublicEventView />} />
+          <Route path="/event/:eventId" element={<PublicEventView />} />
+          
+          {/* Rotas Admin (protegidas) */}
+          <Route 
+            path="/admin/*" 
+            element={
+              <Layout>
+                <Routes>
+                  <Route path="events-list" element={<EventsList />} />
+                  <Route path="create-event" element={<Events />} />
+                  <Route path="events/:id" element={<EventDetail />} />
+                </Routes>
+              </Layout>
+            } 
+          />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
